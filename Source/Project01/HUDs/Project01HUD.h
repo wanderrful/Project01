e62d8c5b09dd66c3720a7ce0b07d1c82ea497943
Wanderrful <wanderrful@gmail.com>
2020-04-23 20:06:54 -0500

    Progress with Interaction System

---
+++ Source/Project01/HUDs/Project01HUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "Project01HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT01_API AProject01HUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
